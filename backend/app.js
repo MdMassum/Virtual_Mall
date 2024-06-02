c847e5a5440e723ebc3184f3e55a2165af8bb4be
Md Emamudin <mdemamudin726@gmail.com>
2024-06-02 21:32:12 +0530

    Implemented_Order_ Apis

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express();
 const bodyparser = require('body-parser');
 const errorMiddleware = require('./middleware/error')
+
+// route imports
 const products = require("./routes/productRoute")
 const users = require("./routes/userRoute")
+const order = require('./routes/orderRoute')
+
 const cookieParser = require('cookie-parser')
 
 // to access req.body
@@ -15,6 +19,7 @@ app.use(cookieParser());
 //available routes
 app.use('/api/v1',products);
 app.use('/api/v1',users);
+app.use('/api/v1',order);
 
 // middleware for errors
 app.use(errorMiddleware)
