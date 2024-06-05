f7edd8ca7af41b504eba0801ab53303cc75f996d
Md Emamudin <mdemamudin726@gmail.com>
2024-06-05 21:22:54 +0530

    Added Home Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
-import Header from './components/layout/Header';
-import { BrowserRouter } from 'react-router-dom';
+import Header from './components/layout/Header/Header';
+import { BrowserRouter ,Routes,Route } from 'react-router-dom';
+import Home from './components/layout/Home/Home';
 
 function App() {
   return (
+    <>
+    
     <BrowserRouter>
         <Header/>
+        <Routes>
+            <Route path="/home" element={<Home/>}></Route>
+            {/* <Route path="/products" element={}></Route>
+            <Route path="/" element={}></Route> */}
+        </Routes>
     </BrowserRouter>
+    </>
     
   );
 }
